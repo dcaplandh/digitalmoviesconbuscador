57fd79d5d95d6e949511077926963af1b0819c5d
dcaplandh <dcaplan@digitalhouse.com>
2018-11-27 19:04:45 -0300

    fetch local

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -3,15 +3,14 @@ window.onload = function(){
   var input = document.querySelector('input[name="search"]');
 
   input.addEventListener('keyup',function(){
-    fetch('http://127.0.0.1:8000/busqueda/'+this.value)
+		
+    fetch('/busqueda/'+this.value)
     .then(function(response){
       return response.json();
     })
     .then(function(data){
-      console.log(data);
       var lista = document.querySelector('ul#busquedas');
       data = Array.from(data);
-      console.log(lista);
       lista.innerHTML = "";
       data.forEach(function(movie){
         lista.innerHTML += '<li class="list-group-item list-group-item-dark">'+movie.title+'</li>';
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -27,7 +27,8 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
+    <form class="form-inline my-2 my-lg-0" method="GET">
+		@csrf
       <input name="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
